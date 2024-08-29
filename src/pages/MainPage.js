3bc5f7585f039fe86d854211f52f8a60f4467f95
Поливода Михаил Александрович <mr.poliwoda2016@yandex.ru>
2024-08-29 22:31:43 +0700

    настроил все для gh-pages

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,12 +5,14 @@ import Search from '../components/Search';
 import Status from '../components/Status';
 import StoryBalance from '../components/StoryBalance';
 import MainContent from '../components/MainContent';
+import logo from '../images/logo.png';
+
 
 function MainPage({ onLogout }) {
     return (
         <div className="all-section">
             <div className="logo-info">
-                <img className="logo" src="/path/to/logo.png" alt="Company's logo" /> 
+            <img className="logo" src={logo} alt="Company's logo" />
                 <Info />
                 <button onClick={onLogout}>Logout</button> {/* Кнопка выхода */}
             </div>
